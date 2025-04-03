9333dafd5a1daa37f4b5d0fc65d4e1bc3fd2867a
admin <admin@gmail.com>
2025-04-03 19:26:13 +0800

    Update analytics

---
+++ src/analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  let hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?3fe661d4a4c01fcbf026cfe5469b1491";
+  let s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
